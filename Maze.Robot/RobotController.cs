af5e9a1168bdabe5e7760ce96477d6f9f6c4d2a0
Manuel Leitner <manuel@leitner.systems>
2017-10-05 19:20:59 +0200

    solved quiz

--- Maze.Robot/RobotController.cs
+++ Maze.Robot/RobotController.cs
@@ -1,20 +1,19 @@
 ﻿using Maze.Library;
+using System.Collections.Generic;
 
-namespace Maze.Solver
-{
-    /// <summary>
-    /// Moves a robot from its current position towards the exit of the maze
-    /// </summary>
-    public class RobotController
-    {
+namespace Maze.Solver {
+    public class RobotController {
+
         private IRobot robot;
+
+        private ISet<(int x, int y)> visited = new HashSet<(int x, int y)>();
+        private bool reachedEnd = false;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="RobotController"/> class
         /// </summary>
         /// <param name="robot">Robot that is controlled</param>
-        public RobotController(IRobot robot)
-        {
+        public RobotController(IRobot robot) {
             // Store robot for later use
             this.robot = robot;
         }
@@ -29,17 +28,38 @@ namespace Maze.Solver
         /// the exit is not reachable, it has to call <see cref="IRobot.HaltAndCatchFire"/>
         /// and exit.
         /// </remarks>
-        public void MoveRobotToExit()
-        {
-            // Here you have to add your code
-
-            // Trivial sample algorithm that can just move right
-            var reachedEnd = false;
+        public void MoveRobotToExit() {
             robot.ReachedExit += (_, __) => reachedEnd = true;
 
-            while (!reachedEnd)
-            {
-                robot.Move(Direction.Right);
+            moveOut(0, 0);
+            if (!reachedEnd) robot.HaltAndCatchFire();
+        }
+
+
+        private void moveOut(int x, int y) {
+            if (!reachedEnd && !visited.Contains((x, y))) {
+                visited.Add((x, y));
+
+                if (robot.TryMove(Direction.Right)) {
+                    moveOut(x + 1, y);
+                    if (!reachedEnd)
+                        robot.Move(Direction.Left);
+                }
+                if (!reachedEnd && robot.TryMove(Direction.Left)) {
+                    moveOut(x - 1, y);
+                    if (!reachedEnd)
+                        robot.Move(Direction.Right);
+                }
+                if (!reachedEnd && robot.TryMove(Direction.Up)) {
+                    moveOut(x, y - 1);
+                    if (!reachedEnd)
+                        robot.Move(Direction.Down);
+                }
+                if (!reachedEnd && robot.TryMove(Direction.Down)) {
+                    moveOut(x, y + 1);
+                    if (!reachedEnd)
+                        robot.Move(Direction.Up);
+                }
             }
         }
     }
